7d85414fe044a196a26e78aaff571f2396cd31af
benny8732511 <benny8732511@gmail.com>
2018-12-04 19:10:44 +0800

    AdafruitDHT.py

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -22,8 +22,30 @@
 import sys
 import time
 import Adafruit_DHT
+import httplib
+import urllib
+import json
+import RPi.GPIO as GPIO
+deviceId="DhlBBf0i"
+deviceKey="E5TTTEIDWWuqkxkj"
 
-
+def post_to_mcs(payload): 
+	headers = {"Content-type": "application/json", "deviceKey": deviceKey} 
+	not_connected = 1 
+	while (not_connected):
+		try:
+			httpClient = httplib.HTTPConnection("api.mediatek.com:80")
+			httpClient.connect() 
+			not_connected = 0 
+		except (httplib.client.HTTPException, socket.error) as ex: 
+			print ("Error: %s" % ex)
+			time.sleep(10)
+			 # sleep 10 seconds 
+	httpClient.request("POST", "/mcs/v2/devices/" + deviceId + "/datapoints", json.dumps(payload), headers) 
+	response = httpClient.getresponse() 
+	print( response.status, response.reason, json.dumps(payload), time.strftime("%c")) 
+	data = response.read() 
+	httpClient.close() 
 # Parse command line parameters.
 sensor_args = { '11': Adafruit_DHT.DHT11,
                 '22': Adafruit_DHT.DHT22,
@@ -35,7 +57,6 @@ else:
     print('Usage: sudo ./Adafruit_DHT.py [11|22|2302] <GPIO pin number>')
     print('Example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO pin #4')
     sys.exit(1)
-
 # Try to grab a sensor reading.  Use the read_retry method which will retry up
 # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
 
@@ -49,10 +70,13 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 # guarantee the timing of calls to read the sensor).
 # If this happens try again!
 while True:
+	humidity, temperature= Adafruit_DHT.read_retry(sensor, pin)
 	if humidity is not None and temperature is not None:
-    		print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+		print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+		payload = {"datapoints":[{"dataChnId":"humi0732","values":{"value":humidity}},{"dataChnId":"temp0732","values":{"value":temperature}}]}
+		post_to_mcs(payload)
 	else:
-    		print('Failed to get reading. Try again!')
-    		sys.exit(1)
-	time.sleep(1)
+		print('Failed to get reading. Try again!')
+		sys.exit(1)
+	time.sleep(10)
 #87
